b7eddb6225850d90737dec0782377d725d8fb33f
pengggan <penggan0@qq.com>
2024-10-09 12:27:37 +0800

    Update youtube_rss.py

--- youtube_rss.py
+++ youtube_rss.py
@@ -22,20 +22,22 @@ TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_YOUTUBE}/sendMess
 
 # RSS 源列表
 RSS_FEEDS = [
-    'https://rsshub.app/zaobao/znews/china', # 联合早报
-   # 'https://rsshub.app/fortunechina',
-    'https://rsshub.app/bilibili/hot-search', # bilibili
-    'https://rss.mifaw.com/articles/5c8bb11a3c41f61efd36683e/5c91d2e23882afa09dff4901', # 36氪 - 24小时热榜
-    'https://rss.mifaw.com/articles/5c8bb11a3c41f61efd36683e/5cac99a7f5648c90ed310e18', # 微博热搜
-    'https://rss.mifaw.com/articles/5c8bb11a3c41f61efd36683e/5cf92d7f0cc93bc69d082608', # 百度热搜榜
-   # 'https://blog.090227.xyz/atom.xml',
-    'https://www.freedidi.com/feed', # 零度解说
-   # 'https://rsshub.app/guancha/headline',
-    'https://rsshub.app/zaobao/znews/china', # 联合早报
-   # 'http://blog.caixin.com/feed',
-   # 'https://rss.mifaw.com/articles/5c8bb11a3c41f61efd36683e/5ca0144af6f83a0a176acfd6',
-    'https://36kr.com/feed',
-    # 添加更多 RSS 源
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCUNciDq-y6I6lEQPeoP-R5A', # 苏恒观察
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCMtXiCoKFrc2ovAGc1eywDg', # 一休
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCNiJNzSkfumLB7bYtXcIEmg', # 真的很博通
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCii04BCvYIdQvshrdNDAcww', # 悟空的日常
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCJMEiNh1HvpopPU3n9vJsMQ', # 理科男士
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCYjB6uufPeHSwuHs8wovLjg', # 中指通
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCSs4A6HYKmHA2MG_0z-F0xw', # 李永乐老师
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCZDgXi7VpKhBJxsPuZcBpgA', # 可恩Ke En
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCSYBgX9pWGiUAcBxjnj6JCQ', # 郭正亮頻道
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCbCCUH8S3yhlm7__rhxR2QQ', # 不良林
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCXkOTZJ743JgVhJWmNV8F3Q', # 寒國人
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UC000Jn3HGeQSwBuX_cLDK8Q', # 我是柳傑克
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCG_gH6S-2ZUOtEw27uIS_QA', # 7Car小七車觀點
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCJ5rBA0z4WFGtUTS83sAb_A', # POP Radio聯播網 官方頻道
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCiwt1aanVMoPYUt_CQYCPQg', # 全球大視野
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCQoagx4VHBw3HkAyzvKEEBA', # 科技共享<
 ]
 
 async def fetch_feed(session, feed, retries=3, delay=10):
@@ -66,20 +68,20 @@ async def send_message(session, chat_id, text):
     except Exception as e:
         logging.error(f"Error sending message to {chat_id}: {e}")
 
-async def process_feed(session, feed, sent_entries, connection):
+async def process_feed(session, feed, sent_youtube, connection):
     feed_data = await fetch_feed(session, feed)
     if feed_data is None:
         return []
 
     new_entries = []
     for entry in feed_data.entries:
-        if entry.link not in sent_entries:
+        if entry.link not in sent_youtube:
             message = f"*{entry.title}*\n{entry.link}"
             for chat_id in ALLOWED_CHAT_IDS:
                 await send_message(session, chat_id, message)
             new_entries.append(entry.link)
             await save_sent_entry_to_db(connection, entry.link)
-            sent_entries.add(entry.link)
+            sent_youtube.add(entry.link)
             await asyncio.sleep(5)
     return new_entries
 
@@ -96,10 +98,10 @@ async def connect_to_db():
         logging.error(f"Error while connecting to MySQL: {e}")
         return None
 
-async def load_sent_entries_from_db(connection):
+async def load_sent_youtube_from_db(connection):
     try:
         async with connection.cursor() as cursor:
-            await cursor.execute("SELECT data FROM sent_entries")
+            await cursor.execute("SELECT data FROM sent_youtube")
             rows = await cursor.fetchall()
             return {json.loads(row[0])['url'] for row in rows}
     except Exception as e:
@@ -109,7 +111,7 @@ async def load_sent_entries_from_db(connection):
 async def save_sent_entry_to_db(connection, url):
     try:
         async with connection.cursor() as cursor:
-            await cursor.execute("INSERT IGNORE INTO sent_entries (data) VALUES (%s)", (json.dumps({"url": url}),))
+            await cursor.execute("INSERT IGNORE INTO sent_youtube (data) VALUES (%s)", (json.dumps({"url": url}),))
             await connection.commit()
             logging.info(f"Saved sent entry: {url}")
     except Exception as e:
@@ -121,11 +123,11 @@ async def main():
         logging.error("Failed to connect to the database. Exiting.")
         return
 
-    sent_entries = await load_sent_entries_from_db(connection)
+    sent_youtube = await load_sent_youtube_from_db(connection)
     new_entries = []
 
     async with aiohttp.ClientSession() as session:
-        tasks = [process_feed(session, feed, sent_entries, connection) for feed in RSS_FEEDS]
+        tasks = [process_feed(session, feed, sent_youtube, connection) for feed in RSS_FEEDS]
         results = await asyncio.gather(*tasks)
         for result in results:
             if result:
